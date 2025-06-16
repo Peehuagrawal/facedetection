de6cab087b5633f36e113928b4931dbc7bd98e64
Peehu_agrawal <166029190+Peehuagrawal@users.noreply.github.com>
2025-06-16 21:31:45 +0530

    initial commit

--- facebasics.py
+++ facebasics.py
@@ -1,1 +1,15 @@
-print("face")
+import cv2
+import mediapipe as mp
+import time
+
+cap= cv2.VideoCapture("faceimages/2.mp4")
+pTime=0
+
+while True:
+    success, img= cap.read()
+    
+    cTime= time.time()
+    fps= 1/(cTime-pTime)
+    cv2.putText(img, f'FPS:{int(fps)}', (28,78), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
+    cv2.imshow("Image", img)
+    cv2.waitKey(10)
