7c758328088bf5872cc1b4acced7859361ca0c22
Peehu_agrawal <166029190+Peehuagrawal@users.noreply.github.com>
2025-06-17 11:55:02 +0530

    initial commit

--- facebasics.py
+++ facebasics.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap= cv2.VideoCapture("faceimages/1.mp4")
+cap= cv2.VideoCapture("faceimages/5.mp4")
 pTime=0
 
 mpFace= mp.solutions.face_detection
@@ -24,11 +24,16 @@ while True:
             #print(detection.location_data.relative_bounding_box)
             bboxC=detection.location_data.relative_bounding_box
             ih,iw,ic= img.shape
-            bbox= 
+            bbox= int(bboxC.xmin *iw), int(bboxC.ymin*ih), \
+            int(bboxC.width*iw), int(bboxC.height*ih)
+            cv2.rectangle(img, bbox,(255,0,255),2)
+
                 
 
     cTime= time.time()
     fps= 1/(cTime-pTime)
+    pTime = cTime
     cv2.putText(img, f'FPS:{int(fps)}', (28,78), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
+    img = cv2.resize(img, (1280, 720))  # or (960, 540) for smaller display
     cv2.imshow("Image", img)
-    cv2.waitKey(20)
+    cv2.waitKey(1)
