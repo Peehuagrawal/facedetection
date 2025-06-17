d940b84914b0f593d03647c0299cfa7c9902c79a
Peehu_agrawal <166029190+Peehuagrawal@users.noreply.github.com>
2025-06-17 12:02:44 +0530

    initial commit

--- facebasics.py
+++ facebasics.py
@@ -2,12 +2,12 @@ import cv2
 import mediapipe as mp
 import time
 
-cap= cv2.VideoCapture("faceimages/5.mp4")
+cap= cv2.VideoCapture("faceimages/1.mp4")
 pTime=0
 
 mpFace= mp.solutions.face_detection
 mpDraw= mp.solutions.drawing_utils
-faceDetection= mpFace.FaceDetection()
+faceDetection= mpFace.FaceDetection(0.75)
 
 while True:
     success, img= cap.read()
@@ -27,8 +27,8 @@ while True:
             bbox= int(bboxC.xmin *iw), int(bboxC.ymin*ih), \
             int(bboxC.width*iw), int(bboxC.height*ih)
             cv2.rectangle(img, bbox,(255,0,255),2)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
 
-                
 
     cTime= time.time()
     fps= 1/(cTime-pTime)
@@ -36,4 +36,6 @@ while True:
     cv2.putText(img, f'FPS:{int(fps)}', (28,78), cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
     img = cv2.resize(img, (1280, 720))  # or (960, 540) for smaller display
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(20)
+    if key == ord('q'):
+        break
